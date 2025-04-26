1498c028750792f70420459d172d70ef56643d1e
Kat892 <104154579+Kat892@users.noreply.github.com>
2025-04-27 01:18:19 +0530

    Add files via upload

---
+++ Greeting.py
@@ -0,0 +1,4 @@
+first_name=input("Enter the First Name : ")
+last_name=input("Enter the Last Name : ")
+full_name=first_name+" "+last_name
+print("Hello,"+" "+full_name+"! Welcome to the Python program.")
---
+++ SImple_Interest.py
@@ -0,0 +1,9 @@
+P=input("Prinicipal: ")
+P=float(P)
+T=input("Time: ")
+T=float(T)
+R=input("Rate: ")
+R=float(R)
+Result=P*T*R/100
+Result=float(Result)
+print("Result: ",Result)
